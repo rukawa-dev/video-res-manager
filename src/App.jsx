e89c5828b81f7ff7491dcea82d98b979546dba72
강석민 <rukawa8188@gmail.com>
2026-02-03 01:01:14 +0900

    배포 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import DetailPage from './pages/DetailPage';
 import Modal from './components/Modal';
